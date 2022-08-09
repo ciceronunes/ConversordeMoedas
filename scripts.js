81fd15ac8c6995e393efaa9656ad057c43d97cf0
ciceronunes <ciceronunescal@edu.unifor.br>
2022-08-08 23:47:47 -0300

    atualização no input

--- scripts.js
+++ scripts.js
@@ -77,5 +77,16 @@ const mudeiPaisDestino = () => {
     }
 }
 
+function k(i) {
+    
+    let v = i.value.replace(/\D/g, '');
+    v = (v / 100).toFixed(2); // + ''
+    v = v.replace(".", ",");
+    v = v.replace(/(\d)(\d{3})(\d{3})(\d{3}),/g, "$1.$2.$3.$4,");
+    v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+    v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+    i.value = v;
+}
+
 button.addEventListener('click', converter)
 select.addEventListener('change', mudeiPaisDestino)
